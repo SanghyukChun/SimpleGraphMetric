1670926f4ba83a6c1b0a0994fbf884e5be88cc6a
Sanghyuk Chun <sanghyuk.chun@gmail.com>
2014-04-15 08:02:34 +0900

    betweenness centrality (not finished)

--- betweennesscentrality.m
+++ betweennesscentrality.m
@@ -4,13 +4,15 @@ function [ betwcent ] = betweennesscentrality( A )
 
 numNode = size(A,1);
 betwcent = zeros(numNode,1);
+A = A > 0;
 
 for node=1:numNode
     for otherNode=1:numNode
-        [numAllPath, numPathIncludeNode] = findshortestpathes(A,node,otherNode);
-        betwcent(node) = betwcent(node) + numPathIncludeNode / numAllPath;
+        [ numAllPath, numPathIncludeNode ] = findshortestpaths(A,node,otherNode);
+        if numAllPath ~= 0
+            betwcent(node) = betwcent(node) + numPathIncludeNode / numAllPath;
+        end
     end
 end
 
 end
-
