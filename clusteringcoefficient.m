652a1d73546fbf9410d57b92441af918914f9f35
Sanghyuk Chun <sanghyuk.chun@gmail.com>
2014-04-15 08:02:18 +0900

    clustering coefficient

--- clusteringcoefficient.m
+++ clusteringcoefficient.m
@@ -1,7 +1,24 @@
-function [ output_args ] = clusteringcoefficient( input_args )
-%CLUSTERINGCOEFFICIENT Summary of this function goes here
-%   Detailed explanation goes here
+function [ cluscoef ] = clusteringcoefficient( A )
+% cluster coefficient of node i is simplely
+% [number of edges between adjacent nodes for node i] /
+% [ (degree of i) * ( (degree of i) -1 ) ]
+%
+% c.f. average cc is sum(C) / n
 
+numNode = size(A,1);
+cluscoef = zeros(numNode,1);
+nodeList = (1:numNode);
 
+A = A > 0;
+degrees = degree(A);
+
+for node=1:numNode
+    if degrees(node) == 1 || degrees(node) == 0
+        continue;
+    end
+    
+    adjNode = nodeList(logical(A(:,node)));
+    n = sum(sum(A(adjNode, adjNode)));
+    
+    cluscoef(node) = n / degrees(node) / (degrees(node)-1);
 end
-
