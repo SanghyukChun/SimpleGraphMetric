10d2b1fcf75d68b0da72d094c255bd2a5a7c2e11
Sanghyuk Chun <sanghyuk.chun@gmail.com>
2014-04-15 08:41:05 +0900

    speed up

--- betweennesscentrality.m
+++ betweennesscentrality.m
@@ -6,9 +6,47 @@ numNode = size(A,1);
 betwcent = zeros(numNode,1);
 A = A > 0;
 
+D = inf(numNode, numNode);
+N = cell(numNode, numNode);
+
+for i=1:numNode
+    D(i,i) = 0;
+end
+
+for i=1:numNode
+    for j=1:numNode
+        if A(i,j) ~= 0
+            D(i,j) = A(i,j);
+        end
+    end
+end
+
+for i=1:numNode
+    for j=1:numNode
+        if A(i,j) ~= 0
+            N{i,j} = j;
+        end
+    end
+end
+
+for k=1:numNode
+    for i=1:numNode
+        for j=1:numNode
+            if D(i,k) + D(k,j) < D(i,j)
+                D(i,j) = D(i,k) + D(k,j);
+                N{i,j} = k;
+            elseif D(i,k) + D(k,j) == D(i,j) && k~=i
+                if ~any(N{i,j} == k) && D(i,j) ~= inf && D(i,j) ~= 0
+                    N{i,j} = [N{i,j}, k];
+                end
+            end
+        end
+    end
+end
+
 for node=1:numNode
     for otherNode=1:numNode
-        [ numAllPath, numPathIncludeNode ] = findshortestpaths(A,node,otherNode);
+        [ numAllPath, numPathIncludeNode ] = findshortestpaths(node,otherNode,N,D);
         disp([num2str(node), ' ' num2str(otherNode)]);
         if numAllPath ~= 0
             betwcent(node) = betwcent(node) + numPathIncludeNode / numAllPath;
--- findshortestpaths.m
+++ findshortestpaths.m
@@ -1,45 +1,5 @@
-function [ numAllPath, numPathIncludeNode ] = findshortestpaths( A,node,otherNode )
+function [ numAllPath, numPathIncludeNode ] = findshortestpaths( node,otherNode,N,D )
 % find shortest path between two vertices using Floyd-Warshall algorithm
-
-A = A > 0;
-numNode = size(A,1);
-D = inf(numNode, numNode);
-N = cell(numNode, numNode);
-
-for i=1:numNode
-    D(i,i) = 0;
-end
-
-for i=1:numNode
-    for j=1:numNode
-        if A(i,j) ~= 0
-            D(i,j) = A(i,j);
-        end
-    end
-end
-
-for i=1:numNode
-    for j=1:numNode
-        if A(i,j) ~= 0
-            N{i,j} = j;
-        end
-    end
-end
-
-for k=1:numNode
-    for i=1:numNode
-        for j=1:numNode
-            if D(i,k) + D(k,j) < D(i,j)
-                D(i,j) = D(i,k) + D(k,j);
-                N{i,j} = k;
-            elseif D(i,k) + D(k,j) == D(i,j) && k~=i
-                if ~any(N{i,j} == k) && D(i,j) ~= inf && D(i,j) ~= 0
-                    N{i,j} = [N{i,j}, k];
-                end
-            end
-        end
-    end
-end
 
 if D(node, otherNode) == inf || node == otherNode
     paths = [];
